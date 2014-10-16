f9535b9a5e451abd2ed4cdcbd624a5d6983fbfb8
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-10-16 22:13:02 +0200

    Add dynamic navigation and content

--- dist/js/main.js
+++ dist/js/main.js
@@ -2409,14 +2409,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
 	 return Blazy;
 });
 
-/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global jQuery:false, document:false */
 'use strict';
-
 (function ($) {
-  $(document).ready(function () {
-    if ($('body').hasClass('lt-ie7')) {return; }
-    // Application specific javascript code goes here
-  }
-  );
+    $(document).ready(function () {
+        if ($('body').hasClass('lt-ie7')) {
+            return;
+        }
+        var bLazy = new Blazy({ selector: '.app-img' });
+    });
 }(jQuery));
--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global jQuery:false, document:false */
 'use strict';
-
 (function ($) {
-  $(document).ready(function () {
-    if ($('body').hasClass('lt-ie7')) {return; }
-    // Application specific javascript code goes here
-  }
-  );
+    $(document).ready(function () {
+        if ($('body').hasClass('lt-ie7')) {
+            return;
+        }
+        var bLazy = new Blazy({ selector: '.app-img' });
+    });
 }(jQuery));
