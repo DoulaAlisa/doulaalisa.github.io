cad8fa6b6915833fa5406b737f0a88285140ce4d
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-12-10 18:39:02 +0100

    Update theme
    - integrate new relative markdoen partial renderer
    - add logo and portait images

--- _plugins/markdown-tag.rb
+++ _plugins/markdown-tag.rb
@@ -7,15 +7,30 @@ module Jekyll
   class MarkdownTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
+      @includes_dir = "_copy"
       @text = text.strip
     end
 
     def render(context)
-      tmpl = File.read File.join Dir.pwd, "_copy", @text
+      dir = resolved_includes_dir(context)
+      file = @text
+      # tmpl = File.read File.join Dir.pwd, "_copy", @text
+      path = File.join(dir, file)
       site = context.registers[:site]
       converter = site.getConverterImpl(Jekyll::Converters::Markdown)
-      tmpl = (Liquid::Template.parse tmpl).render site.site_payload
+      tmpl = (Liquid::Template.parse(source(path, context))).render site.site_payload
       html = converter.convert(tmpl)
+    end
+    def resolved_includes_dir(context)
+      File.join(File.realpath(context.registers[:site].source), @includes_dir)
+    end
+    # Grab file read opts in the context
+    def file_read_opts(context)
+      context.registers[:site].file_read_opts
+    end
+    # This method allows to modify the file content by inheriting from the class.
+    def source(file, context)
+      File.read(file, file_read_opts(context))
     end
   end
 end
