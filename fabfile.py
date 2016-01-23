f5a0af45cdfef23bddc387e37bd8607fe99712eb
Christoph Boehner <cb@vorwaerts-werbung.de>
2016-01-23 22:19:09 +0100

    Refactor fab task

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,6 @@ from fabric.api import env
 from fabric.api import run
 from fabric.api import local
 
-from slacker import Slacker
 
 env.use_ssh_config = True
 env.forward_agent = True
@@ -17,8 +16,6 @@ env.sitename = 'dae'
 env.code_user = 'root'
 env.prod_user = 'www'
 
-slack = Slacker('xoxp-2440800772-2440800774-2520374751-468e84')
-
 
 @task
 def deploy():
@@ -26,7 +23,3 @@ def deploy():
     local('git push')
     with cd(env.code_root):
         run('nice git pull')
-    msg = '[s1] Christoph Boehner updated dae site'
-    user = 'fabric:'
-    icon = ':shipit:'
-    slack.chat.post_message('#general', msg, username=user, icon_emoji=icon)
